062f0b81f8465031796dc66bf9083b99225ecb61
demofortest90-eng <demofortest90@gmail.com>
2026-06-25 23:34:13 +0600

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Add project root to path so app.py can be imported
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
+
+# Vercel expects the WSGI app to be named 'app'
